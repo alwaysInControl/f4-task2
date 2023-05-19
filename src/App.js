cb4357062e5aab314cdc0dd703d6bfb5c3219f12
alwaysInControl <127772513+alwaysInControl@users.noreply.github.com>
2023-05-19 23:06:08 +0600

    task2, state

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class ChangeColor extends React.Component {
+  constructor(props) {
+      super(props);
+      this.state = {
+        isRed: true
+      }
+  }
+
+  SquareClick = () => {
+    this.setState(({ isRed }) => ({ isRed: !isRed }))
+  }
+
+  render() {
+    return (
+      <div className='block'>
+        <div
+          className={`square ${this.state.isRed ? 'red' : 'blue'}`}
+          onClick={this.SquareClick}
+        ></div>
+        <div
+          className={`square ${this.state.isRed ? 'blue' : 'red'}`}
+          onClick={this.SquareClick}
+        ></div>
+      </div>
+    );
+  }
 }
 
-export default App;
+export default ChangeColor;
